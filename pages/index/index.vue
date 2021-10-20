aa26357f19cbf58ae06fc7ad9f16c38012b44889
unknown <416212316@qq.com>
2021-10-20 21:28:40 +0800

    login

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,6 +3,7 @@
 		<image class="logo" src="/static/logo.png"></image>
 		<view class="text-area">
 			<text class="title">{{title}}</text>
+			<button open-type="getUserInfo" @getuserinfo="login">登录</button>
 		</view>
 	</view>
 </template>
@@ -18,7 +19,32 @@
 
 		},
 		methods: {
+			login() {
+				// uni.showModal({
+				// 	title: '温馨提示',
+				// 	content: '亲，授权微信登录后才能正常使用小程序功能',
+				// 	success(res) {
+				// 		if (res.confirm) {
 
+				// 		}
+				// 	}
+				// })
+
+							uni.login({
+								provider: 'weixin',
+								success: function(loginRes) {
+									console.log('loginRes.authResult',loginRes);
+								}
+							});
+							uni.getUserProfile({
+								desc: "注册用户信息使用",
+								lang: "zh_CN",
+								success:(res) => {
+									console.log('res', res)
+								}
+							})
+				
+			}
 		}
 	}
 </script>
